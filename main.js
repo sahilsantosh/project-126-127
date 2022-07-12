ba143e7b41a684c9b703a77faa0df74b4e9191a7
sahilsantosh <108314838+sahilsantosh@users.noreply.github.com>
2022-07-12 08:30:11 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -23,6 +23,49 @@ function modelLoaded() {
 }
 function draw() {
     image(video,0,0, 600, 500);
+
+    Fill("#FF0000");
+    stroke("#FF0000");
+if(scoreRightWrist > 0.2)
+{
+    circle(rightWristX,rightWristY,20);
+
+    if(rightWristY >0 && rightWristY <= 100)
+    {
+        document.getElementById("speed").innerHTML = "speed = 0.5x";
+        song.rate(0.5);
+    }
+    else if(rightWristY  >100 && rightWristY <= 200)
+    {
+        document.getElementById("speed").innerHTML = "speed = 1x";
+        song.rate(1);
+    }
+    else if(rightWristY  >200 && rightWristY <= 300)
+    {
+        document.getElementById("speed").innerHTML = "speed = 1.5x";
+        song.rate(1.5);
+    }
+    else if(rightWristY  >300 && rightWristY <= 400)
+    {
+        document.getElementById("speed").innerHTML = "speed = 2x";
+        song.rate(2);
+    }
+    else if(rightWristY  >400 && rightWristY <= 500)
+    {
+        document.getElementById("speed").innerHTML = "speed = 2.5x";
+        song.rate(2.5);
+    }
+}
+
+  if(scoreLeftWrist > 0.2)  
+  {
+    circle(leftWristX,leftWristY,20);
+    InNumberleftWristY = Number(leftWristY);
+    remove_decimals = floor(InNumberleftWristY);
+    Volume = remove_decimals/500;
+    document.getElementById("volume").innerHTML = "volume = " + Volume;
+    song.setVolume(volume);
+   }
 }
 
 function play()
@@ -37,6 +80,10 @@ function gotPoses(results)
     if(results.length > 0)
     {
         console.log(results);
+        scoreRightWrist = results[0].pose.keypoints[10].score;
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
+        console.log("scoreRightWrist = " +scoreRightWrist + "scoreLeftWrist = " + scoreLeftWrist);
+
         leftWristX=results[0].pose.leftWrist.x;
         leftWristY=results[0].pose.leftWrist.y;
         console.log("leftWristX = " + leftWristX +"leftWristY ="+ leftWristY)
